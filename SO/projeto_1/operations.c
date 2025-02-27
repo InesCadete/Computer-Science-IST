f016f8ca1e8f2c29dd85efebde556f6d0cb403f1
ines <ines.cadete@tecnico.ulisboa.pt>
2025-02-27 08:02:33 +0000

    unzipped folder

---
+++ SO/projeto_1/operations.c
@@ -0,0 +1,376 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <string.h>
+#include <dirent.h>
+#include <time.h>
+#include <stdint.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <pthread.h>
+#include <stdbool.h>
+
+#include "kvs.h"
+#include "constants.h"
+
+
+static struct HashTable* kvs_table = NULL;
+
+extern int simultaneous_backups; 
+extern pthread_mutex_t simultaneous_backups_mutex;
+
+extern int max_backup;
+extern int max_threads;
+
+
+
+typedef struct {
+    char job_file_name[MAX_JOB_FILE_NAME_SIZE];
+    int backup_count;
+} JobBackup;
+
+JobBackup backup_tracker[MAX_JOB_FILES];  // Array to store job files and their counts
+int backup_tracker_size = 0;   // number of job files
+
+/// Calculates a timespec from a delay in milliseconds.
+/// @param delay_ms Delay in milliseconds.
+/// @return Timespec with the given delay.
+static struct timespec delay_to_timespec(unsigned int delay_ms) {
+  return (struct timespec){delay_ms / 1000, (delay_ms % 1000) * 1000000};
+}
+
+int mywrite(int fd, char *buffer) {
+
+    int len =  (int) strlen(buffer);
+    int done = 0;
+
+    while (len > done) {
+        int bytes_written = (int) write(fd, buffer + done, (size_t) (len - done));
+
+        if (bytes_written < 0) {
+            perror("write error");
+            return -1;
+        }
+        done += bytes_written;
+    }
+    return done;
+}
+
+int kvs_init() {
+  if (kvs_table != NULL) {
+    fprintf(stderr, "KVS state has already been initialized\n");
+    return 1;
+  }
+
+  kvs_table = create_hash_table();
+  return kvs_table == NULL;
+}
+
+int kvs_terminate() {
+  if (kvs_table == NULL) {
+    fprintf(stderr, "KVS state must be initialized\n");
+    return 1;
+  }
+
+  //destroy all the locks
+  for(int i = 0; i < TABLE_SIZE; i++) {
+      pthread_rwlock_destroy(&kvs_table->locks[i]);
+    }
+
+  free_table(kvs_table);
+  return 0;
+}
+
+void lock_keys(size_t num_pairs, char keys[][MAX_STRING_SIZE], int mode) {
+
+  int index;
+  bool boolean_locks[TABLE_SIZE] = {false}; // All elements will be initialized to false
+  
+  //filter duplicates while filling the boolean_locks
+  for(size_t i = 0; i < num_pairs; i++) {
+
+    index = hash(keys[i]);
+    if (boolean_locks[index] == false) {
+      boolean_locks[index] = true;
+    }
+  }
+
+  for(int i = 0; i < TABLE_SIZE; i++) {
+
+    if (boolean_locks[i] == true) {
+      if(mode == READ) {
+        pthread_rwlock_rdlock(&kvs_table->locks[i]);
+
+      } else if(mode == WRITE) {
+        pthread_rwlock_wrlock(&kvs_table->locks[i]);
+      }
+    }
+  }
+}
+
+void unlock_keys(size_t num_pairs, char keys[][MAX_STRING_SIZE]) {
+
+  int index;
+  bool boolean_locks[TABLE_SIZE] = {false}; 
+  
+  //filter duplicates while filling the boolean_locks
+  for(size_t i = 0; i < num_pairs; i++) {
+
+    index = hash(keys[i]);
+    if (boolean_locks[index] == false) {
+      boolean_locks[index] = true;
+    }
+  }
+
+  for(int i = 0; i < TABLE_SIZE; i++) {
+
+    if (boolean_locks[i] == true) {
+      pthread_rwlock_unlock(&kvs_table->locks[i]);
+    }
+  }
+}
+//////////////////////////////////////////////////
+int compare_keys(const void *a, const void *b) {
+    const char *key_a = (const char *)a;
+    const char *key_b = (const char *)b;
+    return strcmp(key_a, key_b);
+}
+
+///////////////////////////////////////////////////
+
+
+  int kvs_write(size_t num_pairs, char keys[][MAX_STRING_SIZE], char values[][MAX_STRING_SIZE]) {
+    if (kvs_table == NULL) {
+      fprintf(stderr, "KVS state must be initialized\n");
+      return 1;
+    }
+
+qsort(keys, num_pairs, MAX_STRING_SIZE, compare_keys);
+
+    lock_keys(num_pairs, keys, WRITE);
+
+    for (size_t i = 0; i < num_pairs; i++) {
+
+      if (write_pair(kvs_table, keys[i], values[i]) != 0) {
+        fprintf(stderr, "Failed to write keypair (%s,%s)\n", keys[i], values[i]);
+      }
+    }
+    unlock_keys(num_pairs,keys);
+    
+    return 0;
+  }
+
+int kvs_read(size_t num_pairs, char keys[][MAX_STRING_SIZE], int fd) {
+  if (kvs_table == NULL) {
+    fprintf(stderr, "KVS state must be initialized\n");
+    return 1;
+  }
+
+qsort(keys, num_pairs, MAX_STRING_SIZE, compare_keys);
+
+  lock_keys(num_pairs, keys, READ);
+
+  char buffer[MAX_PIPE];                          
+  
+  sprintf(buffer,"[");     
+  mywrite(fd, buffer);
+  for (size_t i = 0; i < num_pairs; i++) {
+    char* result = read_pair(kvs_table, keys[i]);
+    if (result == NULL) {
+
+      sprintf(buffer, "(%s,KVSERROR)", keys[i]);     
+      mywrite(fd, buffer);
+    } else {
+     sprintf(buffer,"(%s,%s)", keys[i], result);     
+      mywrite(fd, buffer);
+
+    }
+    free(result);
+  }
+
+  sprintf(buffer,"]\n");     
+  mywrite(fd, buffer);
+
+
+ unlock_keys(num_pairs,keys);
+
+  return 0;
+}
+
+int kvs_delete(size_t num_pairs, char keys[][MAX_STRING_SIZE], int fd) {
+  if (kvs_table == NULL) {
+    fprintf(stderr, "KVS state must be initialized\n");
+    return 1;
+  }
+
+qsort(keys, num_pairs, MAX_STRING_SIZE, compare_keys);
+
+  lock_keys(num_pairs, keys, WRITE);
+
+  int aux = 0;
+
+  char buffer[MAX_PIPE];
+
+  for (size_t i = 0; i < num_pairs; i++) {
+    if (delete_pair(kvs_table, keys[i]) != 0) {
+      if (!aux) {
+        sprintf(buffer,"[");     
+        mywrite(fd, buffer);
+
+        aux = 1;
+      }
+      sprintf(buffer,"(%s,KVSMISSING)", keys[i]);     
+      mywrite(fd, buffer);
+    }
+  }
+  if (aux) {
+    sprintf(buffer,"]\n");     
+    mywrite(fd, buffer);
+  }
+
+  unlock_keys(num_pairs,keys);
+
+  return 0;
+}
+
+void kvs_show(int fd) {
+
+  // lock the whole table
+  for(int i = 0; i < TABLE_SIZE; i++) {
+      pthread_rwlock_rdlock(&kvs_table->locks[i]);
+    }
+
+  for (int i = 0; i < TABLE_SIZE; i++) {
+    KeyNode *keyNode = kvs_table->table[i];
+  
+    while (keyNode != NULL) {
+
+      char buffer[MAX_PIPE];                          
+      sprintf(buffer,  "(%s, %s)\n", keyNode->key, keyNode->value);          
+      write(fd, buffer, strlen(buffer));
+
+      keyNode = keyNode->next; // Move to the next node
+    }
+  }
+
+  for(int i = 0; i < TABLE_SIZE; i++) {
+      pthread_rwlock_unlock(&kvs_table->locks[i]);
+    }
+
+  }
+
+/// Waits for the last backup to be called.
+void kvs_wait_backup() {
+    wait(NULL); // Wait for any child process to terminate
+    pthread_mutex_lock(&simultaneous_backups_mutex); // Lock the mutex
+
+    simultaneous_backups --; // Decrement the active backup counter
+    pthread_mutex_unlock(&simultaneous_backups_mutex); // Unlock the mutex
+
+}
+
+int get_backup_count(const char *job_file_name) {
+    for (int i = 0; i < MAX_JOB_FILES; i++) {
+
+        if (strcmp(backup_tracker[i].job_file_name, job_file_name) == 0) {
+            backup_tracker[i].backup_count++;
+          
+            return backup_tracker[i].backup_count;
+        }
+    }
+  
+    // If not found, add a new entry
+    if (backup_tracker_size < MAX_JOB_FILES) {
+        strncpy(backup_tracker[backup_tracker_size].job_file_name, job_file_name, MAX_JOB_FILE_NAME_SIZE - 1);
+        backup_tracker[backup_tracker_size].job_file_name[MAX_JOB_FILE_NAME_SIZE - 1] = '\0'; // Null-terminate
+        backup_tracker[backup_tracker_size].backup_count = 1;
+
+        backup_tracker_size ++;
+
+        return backup_tracker[backup_tracker_size - 1].backup_count;
+    }
+
+    fprintf(stderr, "Error: Backup tracker full.\n");
+    return -1;
+}
+
+int gen_path_backup(char* dir_name, struct dirent* entry, char *in_path, char *out_path) {
+
+    if (!dir_name || !entry || !in_path || !out_path) {
+        return 1; 
+    }
+    // Check the total length of dir_name and entry->d_name
+    size_t dir_len = strlen(dir_name);
+    size_t file_len = strlen(entry->d_name);
+
+    // Ensure the combined path fits within MAX_JOB_FILE_NAME_SIZE
+    if (dir_len + 1 + file_len + 1 > MAX_JOB_FILE_NAME_SIZE) {
+        fprintf(stderr, "Error: Combined path length exceeds MAX_JOB_FILE_NAME_SIZE.\n");
+        return 1;
+    }
+
+    // Copy directory name to in_path
+    strncpy(in_path, dir_name, MAX_JOB_FILE_NAME_SIZE - 1);
+    in_path[MAX_JOB_FILE_NAME_SIZE - 1] = '\0';  // Ensure null termination
+
+    strncat(in_path, "/", MAX_JOB_FILE_NAME_SIZE - strlen(in_path) - 1);
+    strncat(in_path, entry->d_name, MAX_JOB_FILE_NAME_SIZE - strlen(in_path) - 1);
+
+  char *ptr_to_dot = strrchr(entry->d_name, '.');
+    if (ptr_to_dot && strcmp(ptr_to_dot, ".job") == 0) {
+
+        int backup_count = get_backup_count(entry->d_name);
+
+        int pid = fork();
+
+        if (pid == 0) {
+
+          if (backup_count == -1) {
+             exit(1);  
+          }
+
+          snprintf(out_path, MAX_JOB_FILE_NAME_SIZE, "%s/%.*s-%d.bck",
+                  dir_name, (int)(ptr_to_dot - entry->d_name), entry->d_name, backup_count); 
+          
+          // Call readFilesLines and redirect output to .out file
+          int out_fd = open(out_path, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+          
+          if (out_fd == -1) {
+              perror("Failed to open output file");
+              exit(1);
+          }
+
+        kvs_show(out_fd);
+        exit(0); 
+
+      } else if (pid > 0) {
+        simultaneous_backups++;
+      }
+    }
+        return 0;
+}
+
+int kvs_backup(char* dir_name, struct dirent* entry, char *in_path, char *out_path) {
+
+    pthread_mutex_lock(&simultaneous_backups_mutex); // Lock the mutex
+
+    if(simultaneous_backups >= max_backup ) {
+      pthread_mutex_unlock(&simultaneous_backups_mutex); // Unlock before waiting
+      kvs_wait_backup();
+      pthread_mutex_lock(&simultaneous_backups_mutex); // Re-lock the mutex
+
+    }
+
+    pthread_mutex_unlock(&simultaneous_backups_mutex); // Unlock the mutex
+
+    gen_path_backup(dir_name, entry, in_path, out_path);
+
+  return 0;
+}
+
+void kvs_wait(unsigned int delay_ms) {
+  struct timespec delay = delay_to_timespec(delay_ms);
+  nanosleep(&delay, NULL);
+}
